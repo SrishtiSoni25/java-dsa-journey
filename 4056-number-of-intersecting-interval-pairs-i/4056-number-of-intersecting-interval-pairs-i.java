bb000dbd1e880b11a2b8adf0406ef177b9108f72
SrishtiSoni25 <128698353+SrishtiSoni25@users.noreply.github.com>
2026-09-21 23:29:01 +0530

    Time: 4 ms (100%), Space: 46.6 MB (100%) - LeetHub

---
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countIntersectingIntervals(int[][] intervals) {
+
+        int n = intervals.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (intervals[i][0] <= intervals[j][1] &&
+                    intervals[j][0] <= intervals[i][1]) {
+
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
